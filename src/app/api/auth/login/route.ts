505cc51776430a5e23b0cf479760273526c13638
Cato4525 <laguilaroyagata@gmail.com>
2026-06-01 18:37:48 -0500

    nuevo modulo productos

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -19,8 +19,8 @@ export async function POST(request: Request) {
     password,
   })
 
-  console.log("URL:", process.env.NEXT_PUBLIC_SUPABASE_URL)
-  console.log("KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+  console.log("SUPABASE_URL:", process.env.NEXT_PUBLIC_SUPABASE_URL)
+  console.log("SUPABASE_KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.slice(0, 10))
 
   if (error) {
     console.error("Supabase login error:", error)
